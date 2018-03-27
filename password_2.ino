b7e3aa01ebfedc73f51bc826e9bcabdf75a2eb63
DanielMay777 <37666058+DanielMay777@users.noreply.github.com>
2018-03-27 13:07:07 +0300

    Add files via upload

---
+++ password_2.ino
@@ -0,0 +1,46 @@
+#include <LiquidCrystal.h>
+String PASSADMIN="0000";
+String PASSW="12345678";
+
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+
+void setup() 
+{
+ Serial.begin(9600);
+ lcd.begin(16,2);
+ 
+ }
+void loop() 
+{
+
+  lcd.print("Enter code");
+  byte string; 
+ if(Serial.available()>0){
+    string=Serial.read();
+    switch (string){
+      case '0000':
+                  lcd.println("Hello Mr. Elay Eitach");
+                  delay(500);
+                  lcd.clear();
+                  lcd.println("No: 0524804696");
+                  delay(500);
+                  lcd.clear();
+                  break;
+      case '12345678':
+                  lcd.println("Hello Mr. Daniel Mayzlin");
+                  delay(500);
+                  lcd.clear();
+                  lcd.println("No: 0547541337");
+                  delay(500);
+                  lcd.clear();
+                  break;
+      default:
+                  lcd.println("Try again"); // lcd clear will be done in the beginning of loop
+                  delay(500);
+//                tries++;
+                  break;   
+    }// end switch
+  }// end if
+}// end loop
+
