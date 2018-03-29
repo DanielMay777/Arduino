ef22c8990c70ef5b6c5b70391636c4e06a0b231d
DanielMay777 <37666058+DanielMay777@users.noreply.github.com>
2018-03-29 23:48:27 +0300

    Add files via upload

---
+++ password3.ino
@@ -0,0 +1,83 @@
+#define analogpin 0 
+#define led_r 10 
+#define led_g 11 
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);
+String password = "abc";
+String passwordAdmin;
+int val;
+int i;
+void setup()
+{
+  Serial.begin(9600);
+ pinMode(analogpin,INPUT);
+pinMode(led_r ,OUTPUT); 
+pinMode(led_g ,OUTPUT);
+digitalWrite(led_r, HIGH);
+digitalWrite(led_g, HIGH);
+}
+void loop()
+{
+  lcd.clear();
+  lcd.print("*Enter Password*");
+  delay(2000);
+  if (Serial.available()>0)
+  {
+    passwordAdmin = Serial.readString();
+  if (password == passwordAdmin)
+    { 
+     digitalWrite(led_r, LOW);
+     digitalWrite(led_g, HIGH);
+     lcd.clear(); 
+     lcd.println("*Welcome Daniel*");
+     delay(2000);
+     lcd.clear();
+     lcd.print("*****Mayzlin****");
+     delay(2000);
+     for(i=0; i>30; i++)
+     {
+      lcd.print("**Door is Open**");
+      delay(2000);
+
+     }
+     digitalWrite(led_r ,HIGH);
+     digitalWrite(led_g ,LOW);
+     lcd.clear();
+     lcd.print("Close The Door");
+     delay(2000);
+     if(val<470){
+     lcd.println("*DOOR IS CLOSED*");
+     delay(100); 
+     lcd.clear();
+    
+}
+     }
+     else 
+     {
+      digitalWrite(led_r ,HIGH);
+      digitalWrite(led_g ,LOW);
+      for (i=0; i<2; i++)
+      {
+      lcd.clear();
+      lcd.println("*FALSE CODE!*");
+      delay(2000);
+      lcd.clear();
+      lcd.println("**TRY AGAIN**");
+      delay(2000);
+      lcd.clear();
+     }
+     lcd.clear();
+     lcd.println("Another Wrong Attempt");
+     delay(2000);
+     lcd.clear();
+     lcd.println("The Siren Turns On!");
+     delay(2000);
+     lcd.clear();
+     lcd.println("SIREN INCLUDED!");
+     }
+  }
+}
+
+
+
+
