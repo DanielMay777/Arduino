1660ec6a2f2d9918270b66ce2f14a79546853bcf
DanielMay777 <37666058+DanielMay777@users.noreply.github.com>
2018-03-31 12:21:54 +0300

    Add files via upload

---
+++ sketch_mar30a.ino
@@ -0,0 +1,21 @@
+String password = "abc";
+String passwordAdmin;
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+if (Serial.available()>0)
+{
+ passwordAdmin = Serial.readString();
+ if (password == passwordAdmin)
+{
+Serial.print("**0547658882");
+delay(1000);
+Serial.print("##Daniel Mayzlin come home");
+delay(1000);
+Serial.print("$$");
+delay(1000);
+}
+}
+}
